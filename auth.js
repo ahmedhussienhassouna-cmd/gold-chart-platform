c66f879c21502cd9a3efaa2a5ed1a82d5e207a8e
ahmedhussienhassouna-cmd <ahmedhussienhassouna@gmail.com>
2026-06-15 04:47:16 +0300

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,91 @@
+// =======================
+// REGISTER
+// =======================
+function registerUser() {
+
+    const name =
+        document.getElementById("registerName").value;
+
+    const email =
+        document.getElementById("registerEmail").value;
+
+    const password =
+        document.getElementById("registerPassword").value;
+
+    if (!name  !email  !password) {
+        alert("Please fill all fields");
+        return;
+    }
+
+    const user = {
+        name,
+        email,
+        password
+    };
+
+    localStorage.setItem(
+        "golden_user",
+        JSON.stringify(user)
+    );
+
+    alert("Account Created Successfully");
+
+    window.location.href = "login.html";
+}
+
+// =======================
+// LOGIN
+// =======================
+function loginUser() {
+
+    const email =
+        document.getElementById("loginEmail").value;
+
+    const password =
+        document.getElementById("loginPassword").value;
+
+    const saved =
+        JSON.parse(localStorage.getItem("golden_user"));
+
+    if (!saved) {
+        alert("No account found");
+        return;
+    }
+
+    if (
+        email === saved.email &&
+        password === saved.password
+    ) {
+
+        localStorage.setItem(
+            "golden_logged",
+            "true"
+        );
+
+        window.location.href =
+            "dashboard.html";
+
+    } else {
+
+        alert("Wrong Email or Password");
+    }
+}
+
+// =======================
+// PROTECT DASHBOARD
+// =======================
+if (
+    window.location.pathname.includes(
+        "dashboard.html"
+    )
+) {
+
+    const logged =
+        localStorage.getItem("golden_logged");
+
+    if (logged !== "true") {
+
+        window.location.href =
+            "login.html";
+    }
+}
