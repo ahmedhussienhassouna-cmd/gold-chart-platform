0e98bddd2cebfa7ed5c7533aab655d79d6ad288c
ahmedhussienhassouna-cmd <ahmedhussienhassouna@gmail.com>
2026-06-15 05:16:09 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,7 +1,10 @@
 alert("AUTH LOADED");
 
+// =======================
 // REGISTER
+// =======================
 function registerUser() {
+
     const name = document.getElementById("registerName").value;
     const email = document.getElementById("registerEmail").value;
     const password = document.getElementById("registerPassword").value;
@@ -20,11 +23,15 @@ function registerUser() {
     localStorage.setItem("golden_user", JSON.stringify(user));
 
     alert("Account Created Successfully");
+
     window.location.href = "login.html";
 }
 
+// =======================
 // LOGIN
+// =======================
 function loginUser() {
+
     const email = document.getElementById("loginEmail").value;
     const password = document.getElementById("loginPassword").value;
 
@@ -36,18 +43,26 @@ function loginUser() {
     }
 
     if (email === saved.email && password === saved.password) {
+
         localStorage.setItem("golden_logged", "true");
+
         window.location.href = "dashboard.html";
+
     } else {
+
         alert("Wrong Email or Password");
     }
 }
 
+// =======================
 // PROTECT DASHBOARD
+// =======================
 if (window.location.pathname.includes("dashboard.html")) {
+
     const logged = localStorage.getItem("golden_logged");
 
     if (logged !== "true") {
+
         window.location.href = "login.html";
     }
 }
