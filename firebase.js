258d15939aa2c7e93714a3aa3d8b00adf2998019
ahmedhussienhassouna-cmd <ahmedhussienhassouna@gmail.com>
2026-06-17 01:24:38 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,38 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import {
+    getFirestore,
+    doc,
+    getDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+const firebaseConfig = {
+    apiKey: "AIzaSyBRpdJ72pV8t2KyVpSyaRc9WDihDa2d4HU",
+    authDomain: "golden-trade-levels.firebaseapp.com",
+    projectId: "golden-trade-levels",
+    storageBucket: "golden-trade-levels.firebasestorage.app",
+    messagingSenderId: "449735025504",
+    appId: "1:449735025504:web:93f4ddae6ca2830977f405"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+window.loadStrategyLevels = async function(){
+
+    try{
+
+        const docRef = doc(db, "strategy", "today");
+        const docSnap = await getDoc(docRef);
+
+        if(!docSnap.exists()){
+            return null;
+        }
+
+        return docSnap.data();
+
+    }catch(error){
+
+        console.error(error);
+        return null;
+    }
+};
